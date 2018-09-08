44a0d45112cdb7270a873e3b52d62402c7363a89
Antonio Aloisio <antonio.aloisio@zalando.fi>
2018-09-08 15:40:45 +0300

    PC target renamed to Dummy

--- target/vita/vita.cmake
+++ target/vita/vita.cmake
@@ -22,52 +22,52 @@ ENDFUNCTION()
 MACRO(INIT_VITA_BUILD TITLE_ID)
     ## Configuration options for this app
     # Display name (under bubble in LiveArea)
-    set(VITA_APP_NAME ${APP_NAME})
+    SET(VITA_APP_NAME ${APP_NAME})
 
     # Unique ID must be exactly 9 characters. Recommended: XXXXYYYYY where X =
     # unique string of developer and Y = a unique number for this app
-    set(VITA_TITLEID TITLE_ID)
+    SET(VITA_TITLEID TITLE_ID)
 
-    add_definitions(-DVITA_TITLEID="${VITA_TITLEID}")
+    ADD_DEFINITIONS(-DVITA_TITLEID="${VITA_TITLEID}")
 
     # Optional version string to show in LiveArea's more info screen
-    option(DEBUGNET "Enable debugnet for logging" ON)
+    OPTION(DEBUGNET "Enable debugnet for logging" ON)
 
-    if (CMAKE_BUILD_TYPE MATCHES DEBUG)
-        message("++ Debug build enabled")
-        if (DEBUGNET)
-            add_definitions(-DDEBUGNET)
-            target_link_libraries(${APP_NAME} debugnet)
-            file(STRINGS "debugnetip.txt" DEBUGNETIP)
-            add_definitions(-DDEBUGNETIP="${DEBUGNETIP}")
-            message("++ DEBUGNET is on. Your app will be sending msg to ${DEBUGNETIP}.")
-        endif ()
-    else (CMAKE_BUILD_TYPE MATCHES DEBUG)
-        message("++ Release build - Run cmake -DCMAKE_BUILD_TYPE=Debug .. to enable debugging")
-    endif (CMAKE_BUILD_TYPE MATCHES DEBUG)
+    IF (CMAKE_BUILD_TYPE MATCHES DEBUG)
+        MESSAGE("++ Debug build enabled")
+        IF (DEBUGNET)
+            ADD_DEFINITIONS(-DDEBUGNET)
+            TARGET_LINK_LIBRARIES(${APP_NAME} debugnet)
+            FILE(STRINGS "debugnetip.txt" DEBUGNETIP)
+            ADD_DEFINITIONS(-DDEBUGNETIP="${DEBUGNETIP}")
+            MESSAGE("++ DEBUGNET is on. Your app will be sending msg to ${DEBUGNETIP}.")
+        ENDIF ()
+    ELSE (CMAKE_BUILD_TYPE MATCHES DEBUG)
+        MESSAGE("++ Release build - Run cmake -DCMAKE_BUILD_TYPE=Debug .. to enable debugging")
+    ENDIF (CMAKE_BUILD_TYPE MATCHES DEBUG)
 
     ## Flags and includes for building
     # Note that we make sure not to overwrite previous flags
-    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wall")
+    SET(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wall")
 
-    file(GLOB_RECURSE res_files RELATIVE ${CMAKE_SOURCE_DIR} assets/*.png assets/*.jpeg assets/*.yml assets/*.ttf assets/*.wav assets/*.ogg)
-    add_resources(PROJECT_RESOURCES ${res_files})
+    FILE(GLOB_RECURSE res_files RELATIVE ${CMAKE_SOURCE_DIR} assets/*.png assets/*.jpeg assets/*.yml assets/*.ttf assets/*.wav assets/*.ogg)
+    ADD_RESOURCES(PROJECT_RESOURCES ${res_files})
 ENDMACRO()
 
 FUNCTION(BUILD_VPK VITA_VERSION VPK_BUNDLED_FILES)
     if (VPK_BUNDLED_FILES)
         # This line adds Vita helper macros, must go after project definition in order
         # to build Vita specific artifacts (self/vpk).
-        include("$ENV{VITASDK}/share/vita.cmake" REQUIRED)
+        INCLUDE("$ENV{VITASDK}/share/vita.cmake" REQUIRED)
         # Optional. You can specify more param.sfo flags this way.
-        set(VITA_MKSFOEX_FLAGS "${VITA_MKSFOEX_FLAGS} -d PARENTAL_LEVEL=1")
+        SET(VITA_MKSFOEX_FLAGS "${VITA_MKSFOEX_FLAGS} -d PARENTAL_LEVEL=1")
         vita_create_self(${APP_NAME}.self ${APP_NAME})
         vita_create_vpk(${APP_NAME}.vpk ${VITA_TITLEID} ${APP_NAME}.self
                 VERSION ${VITA_VERSION}
                 NAME ${VITA_APP_NAME}
                 ${VPK_BUNDLED_FILES})
     else()
-        message("Please define VPK_BUNDLED_FILES as list of files")
+        MESSAGE("Please define VPK_BUNDLED_FILES as list of files")
     endif()
 
 
